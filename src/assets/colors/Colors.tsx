d6e3ceb7fd79bcfba878966d578b3611f4fc650c
NightfuryEquinn <xianzyip8@gmail.com>
2023-12-11 23:18:13 +0800

    Added assets

---
+++ src/assets/colors/Colors.tsx
@@ -0,0 +1,6 @@
+export const colors = {
+  white: "#F4F4F9",
+  skyblue: "#B8DBD9",
+  greyblue: "#586F7C",
+  deepblue: "#334753"
+}
--- src/assets/styles/AppStyles.tsx
+++ src/assets/styles/AppStyles.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../colors/Colors";
 
 export const AppStyles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: colors.greyblue
   },
   title: {
     fontSize: 32,
